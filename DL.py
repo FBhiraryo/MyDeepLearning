d1014c4e093616f8696dc93c9b104f7fd207fbe1
FBhiraryo <fbhr05@outlook.jp>
2025-09-07 20:27:09 +0900

    層とノードを増やした

--- DL.py
+++ DL.py
@@ -12,8 +12,12 @@ y = dataset[:,8]
 
 # define the keras model
 model = Sequential()
-model.add(Dense(12, input_shape=(8,), activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(40, input_shape=(8,), activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(20, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile the keras model
